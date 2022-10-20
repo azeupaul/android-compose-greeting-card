deba76d562d41635c722e820766a9f3b99518beb
azeupaul <azeupaul@gmail.com>
2022-10-20 22:51:20 +0100

    Add text composable

--- app/src/main/java/com/azeupaul/greetingcard/MainActivity.kt
+++ app/src/main/java/com/azeupaul/greetingcard/MainActivity.kt
@@ -3,16 +3,15 @@ package com.azeupaul.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.azeupaul.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithText( "Happy Birthday Said!", "-from Pablo")
                 }
             }
         }
@@ -33,16 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Magenta) {
-        Text(text = "Hi, my name is $name!", modifier = Modifier.padding(24.dp))
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun BirthdayCardPreview() {
     GreetingCardTheme {
-        Greeting("Pablo")
+        BirthdayGreetingWithText( "Happy Birthday Said!", "-from Pablo")
     }
 }
