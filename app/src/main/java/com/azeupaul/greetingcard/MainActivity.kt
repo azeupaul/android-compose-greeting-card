af9d5fff0053919950c31ee214168eb9cca7383c
azeupaul <azeupaul@gmail.com>
2022-10-20 23:30:25 +0100

    Add images and align the text

--- app/src/main/java/com/azeupaul/greetingcard/MainActivity.kt
+++ app/src/main/java/com/azeupaul/greetingcard/MainActivity.kt
@@ -3,14 +3,18 @@ package com.azeupaul.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.azeupaul.greetingcard.ui.theme.GreetingCardTheme
 
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText( "Happy Birthday Said!", "-from Pablo")
+                    BirthdayGreetingWithImage( "Happy Birthday Said!", "-from Pablo")
                 }
             }
         }
@@ -36,12 +40,36 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     Column {
         Text(
             text = message,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
         )
         Text(
             text = from,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
         )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message, from)
     }
 }
 
@@ -49,6 +77,6 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 @Composable
 fun BirthdayCardPreview() {
     GreetingCardTheme {
-        BirthdayGreetingWithText( "Happy Birthday Said!", "-from Pablo")
+        BirthdayGreetingWithImage( "Happy Birthday Said!", "- from Pablo")
     }
 }
